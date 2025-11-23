a0a328aebaff3e64eac4fb19ecc20ee95367d5f1
yashika0128 <chaubeyyashika3@gmail.com>
2025-11-23 16:22:11 +0530

    Reverse Of A Number

---
+++ src/Lecture03/ReverseOfANumber.java
@@ -0,0 +1,16 @@
+package Lecture03;
+import java.util.Scanner;
+public class ReverseOfANumber {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+
+        int r= 0;
+        while(n!=0){
+            r *=10;
+            r += n%10;
+            n /=10;
+        }
+        System.out.println(r);
+    }
+}
