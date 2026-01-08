50ba16c2bb87ca10f5de3e1fde7398e80737135f
yashika0128 <chaubeyyashika3@gmail.com>
2026-01-08 20:15:39 +0530

    LeetCode_1480_Running_Sum_of_1d_Array

---
+++ src/Array/LeetCode_1480_Running_Sum_of_1d_Array.java
@@ -0,0 +1,15 @@
+package Array;
+public class LeetCode_1480_Running_Sum_of_1d_Array {
+    public static void main(String[] args) {
+//        class Solution {
+//            public int[] runningSum(int[] nums) {
+//                int n= nums.length;
+//                for(int i=1;i<n;i++){
+//                    nums[i] =nums[i]+nums[i-1];
+//                }
+//                return nums;
+//            }
+//
+//        }
+    }
+}
