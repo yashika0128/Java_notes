1bb3f9973e4f7eb076a3fd712b85a95531c1b761
yashika0128 <chaubeyyashika3@gmail.com>
2026-01-10 22:43:58 +0530

    Product of matrix (2D array)

---
+++ src/_2D_Array/Product.java
@@ -0,0 +1,51 @@
+package _2D_Array;
+import java.util.Scanner;
+public class Product {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int r1= sc.nextInt();
+        int c1= sc.nextInt();
+        int [][] arr1= new int[r1][c1];
+        for (int i = 0; i <arr1.length; i++) {
+            for (int j = 0; j < arr1[0].length; j++) {
+                arr1[i][j]= sc.nextInt();
+            }
+        }
+
+        int r2= sc.nextInt();
+        int c2= sc.nextInt();
+        int [][] arr2 = new int[r2][c2];
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                arr2[i][j]= sc.nextInt();
+            }
+        }
+
+        if (c1 != r2){
+            System.out.println("Invalid Input. \n Cannot be multiplied");
+            return;
+        }
+
+        int [][] result = new int[r1][c2];
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                for (int k = 0; k < c1; k++) {
+                    result[i][j] += arr1[i][k]*arr2[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                System.out.print(result[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+
+
+
+
+    }
+}
