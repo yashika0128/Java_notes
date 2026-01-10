b47133a16b6cf888eea565a7d3d284ecf3725b09
yashika0128 <chaubeyyashika3@gmail.com>
2026-01-10 22:48:36 +0530

    Leetcode 121- Buy ans sell stocks

---
+++ src/Array/Buy_sell_stocks.java
@@ -0,0 +1,28 @@
+package Array;
+
+public class Buy_sell_stocks {
+    public static void main(String[] args) {
+//        class Solution {
+//            public int maxProfit(int[] prices) {
+//                int n = prices.length;
+//                // int maxprofit = 0;
+//                // for(int i=0;i<n;i++){
+//                //     for(int j=i+1;j<n;j++){
+//                //         int profit = prices[j]-prices[i];
+//                //         maxprofit = Math.max(profit,maxprofit);
+//                //     }
+//                // }
+//                // return maxprofit;
+//
+//                int sb = prices[0];
+//                int maxprofit = 0;
+//                for(int i=0;i<n;i++){
+//                    int profit = prices[i]-sb;
+//                    maxprofit = Math.max(profit,maxprofit);
+//                    sb= Math.min(prices[i],sb);
+//                }
+//                return maxprofit;
+//            }
+//        }
+    }
+}
