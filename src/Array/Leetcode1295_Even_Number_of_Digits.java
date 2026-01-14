0fe021cddf1f4650d751835864a7a567073297ce
yashika0128 <chaubeyyashika3@gmail.com>
2026-01-14 14:44:23 +0530

    Leetcode 1295: Find Numbers with Even_Number_of_Digits

---
+++ src/Array/Leetcode1295_Even_Number_of_Digits.java
@@ -0,0 +1,31 @@
+package Array;
+
+public class Leetcode1295_Even_Number_of_Digits {
+    public static void main(String[] args) {
+        class Solution {
+            public int findNumbers(int[] nums) {
+                int final_count = 0;
+                // for(int i=0;i<nums.length;i++){
+                //     int count=0;
+                //     while(nums[i]!=0){
+                //         int temp= nums[i]%10;
+                //         nums[i] /=10;
+                //         count++;
+                //     }
+                //     if(count %2 == 0)final_count++;
+                // }
+
+                for(int i=0; i<nums.length;i++){
+                    int count=0;
+                    int num= nums[i];
+                    while(num!=0){
+                        num /= 10;
+                        count ++;
+                    }
+                    if(count %2 ==0) final_count ++;
+                }
+                return final_count;
+            }
+        }
+    }
+}
