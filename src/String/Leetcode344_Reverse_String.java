e69071ce72e1d9e188cc2b5a6fb0152b2fc9cf6f
yashika0128 <chaubeyyashika3@gmail.com>
2026-01-15 15:20:19 +0530

    Leetcode:344_Reverse_String

---
+++ src/String/Leetcode344_Reverse_String.java
@@ -0,0 +1,26 @@
+package String;
+
+public class Leetcode344_Reverse_String {
+    public static void main(String[] args) {
+        class Solution {
+            public void reverseString(char[] s) {
+                // int i=0;
+                // int j= s.length-1;
+                // while(i<j){
+                //     char temp = s[i];
+                //     s[i]= s[j];
+                //     s[j]= temp;
+                //     i++;
+                //     j--;
+                // }
+
+                for (int i = 0, j = s.length - 1; i < j; i++, j--) {
+                    char temp = s[i];
+                    s[i] = s[j];
+                    s[j] = temp;
+                }
+                return;
+            }
+        }
+    }
+}
