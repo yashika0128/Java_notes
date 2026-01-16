7cf3b21e89bac414c181ac0e4fa2825a28cfbef6
yashika0128 <chaubeyyashika3@gmail.com>
2026-01-16 15:08:27 +0530

    Count the number of Prime Numbers in the array

---
+++ src/Array/Count_PrimeNumber.java
@@ -0,0 +1,31 @@
+package Array;
+import java.util.Scanner;
+
+public class Count_PrimeNumber {
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (isPrime(arr[i])) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
