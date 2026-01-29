f68d083a048d374a702ff6f68ceb14ee9b28ffd1
yashika0128 <chaubeyyashika3@gmail.com>
2026-01-29 11:15:01 +0530

    Factorial_of_num using recursion

--- src/Recursion/Factorial_of_num.java
+++ src/Recursion/Factorial_of_num.java
@@ -1,9 +1,11 @@
 package Recursion;
-
+import java.util.Scanner;
 public class Factorial_of_num {
     public static void main(String[] args) {
-        int n=4;
-        System.out.println(fact(5));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to get it's factorial: ");
+        int n=sc.nextInt();
+        System.out.println("Factorial of "+n+" is: "+fact(n));
     }
     public static int fact(int n){
         // Base Case
